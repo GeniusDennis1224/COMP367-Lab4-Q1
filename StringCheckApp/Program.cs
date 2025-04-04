16b2d0a26de262edbc29013d573746dfa77f32c9
Dennis Liu <genius_dennis1224@hotmail.com>
2025-04-04 15:25:34 -0400

    Add new project StringCheckApp into solution

---
+++ StringCheckApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using StringLibrary;
+
+namespace StringCheckApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(input.StartsWithUpperCase()
+                ? "✅ Starts with uppercase letter."
+                : "❌ Does NOT start with uppercase letter.");
+        }
+    }
+}
